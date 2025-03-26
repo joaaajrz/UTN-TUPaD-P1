aee62a9776c1e21923d3dfa5528cb7b43434aed5
Joaquin Juarez <joaquinignaciojuarez@gmail.com>
2025-03-26 16:06:51 -0300

    Se subio TP2

---
+++ 02 Trabajo Colaborativo/JUAREZ-JOAQUIN_TP-02.py
@@ -0,0 +1,112 @@
+# 1) Contestar las siguientes preguntas utilizando las guías y documentación proporcionada
+
+# • ¿Qué es GitHub?
+#Respuesta: GitHub es una pagina donde podemos compartir nuestros repositorios con otras personas.
+
+
+# • ¿Cómo crear un repositorio en GitHub?
+#Respuesta: Para crear un repositorio en GitHub necesitaremos haber creado un repositorio local previamente. Luego nos dirigiremos a 'github.com', en la parte derecha superior encontraremos el signo de mas, apretamos y eligiremos la opcion 'New repository'.
+
+
+# • ¿Cómo crear una rama en Git?
+#Respuesta: Para crear una rama en git, debemos usar el comando 'git branch [nombreRama].
+
+
+# • ¿Cómo cambiar a una rama en Git?
+#Respuesta: Para cambiar de rama debemos usar el comando 'git checkout [nombreRama]'.
+
+
+# • ¿Cómo fusionar ramas en Git?
+#Respuesta: Para fusionar ramas debemos posicionarnos en una rama y utilizar el comando 'git merge [nombreRama2]'.
+
+
+# • ¿Cómo crear un commit en Git?
+#Respuesta: Para crear un commit utilizaremos el comando 'git commit'. Podremos incluir un mensaje agregandole al comando '-m "[mensaje]"'.
+
+
+# • ¿Cómo enviar un commit a GitHub?
+#Respuesta: Debemos crear el commit y luego poner el comando 'git push'.
+
+
+# • ¿Qué es un repositorio remoto?
+#Respuesta: Un repositorio remoto es un repositorio subido a internet para que otras personas puedan trabajar en el.
+
+
+# • ¿Cómo agregar un repositorio remoto a Git?
+#Respuesta: Para agregar un repositorio remoto a Git debemos entrar a una carpeta desde la terminal, luego de iniciar el repositorio con el comando 'git init' y por ultimo poner el comando 'git clone [URL del repositorio]'.
+
+
+# • ¿Cómo empujar cambios a un repositorio remoto?
+#Respuesta: En primer lugar hay que guardar los cambios, en segundo luga, si es nuestra primera vez debemos introducir en la terminal el comando 'git push -u origin [nombreRama]' pero si es nuestra segunda vez bastara con el comando 'git push'.
+
+
+# • ¿Cómo tirar de cambios de un repositorio remoto?
+#Respuesta: Para tirar de cambios de un repositorio remoto, utilizaremos el comando 'git pull origin [nombreRama]'.
+
+
+# • ¿Qué es un fork de repositorio?
+#Respuesta: Un fork es una copia del repositorio remoto de otro usuario en nuestra cuenta, donde nosotros lo podremos editar sin interferir en lel repositorio del otro.
+
+
+# • ¿Cómo crear un fork de un repositorio? 
+#Respuesta: Para crear un fork debemos dirigirnos a la pagina donde se encuentra el repositorio en GitHub y presionar el boton que dice fork. Despues de que se haga la copia del repositorio en nuestra cuenta, debemos clonarlo, para empezar a trabajar.
+
+
+# • ¿Cómo enviar una solicitud de extracción (pull request) a un repositorio?
+#Respuesta: Para enviar una solicitud de extraccion debemos crear un fork, clonar el repositorio del fork en nuestra pc, crear una nueva rama y hacer los cambios que creamos necesarios. Cuando terminemos subiremos a GitHub. Por ultimo debemos dirigirnos a nuestro fork en 'github.com' y nos saldra un aviso y seleccionaremos 'Compare & pull request'.
+
+
+# • ¿Cómo aceptar una solicitud de extracción?
+#Respuesta: Debemos dirigirnos a nuestro repositrio en GitHub y en la barra de navegacion seleccionar la opcion 'pull request', donde nos aparecera las solicitudes que nos han hecho, y luego de analizar y estar seguros clickearemos el boton que dice 'merge pull request'.
+
+
+# • ¿Qué es un etiqueta en Git?
+#Respuesta: Una etiqueta es una forma de marcar un punto especifico en el historial de nuestro repositorio.
+
+
+# • ¿Cómo crear una etiqueta en Git?
+#Respuesta: Para crear una etiqueta usaremos el comando 'git tag'.
+
+
+# • ¿Cómo enviar una etiqueta a GitHub?
+#Respuesta: Para enviar una etiqueta a GitHub debes usar el comando 'git push origin [etiqueta]' o 'git push --tags'.
+
+
+# • ¿Qué es un historial de Git?
+#Respuesta: El historial de git es el registro de de cambios de un repositorio.
+
+
+# • ¿Cómo ver el historial de Git?
+#Respuesta: Para ver el historia de Git usaremos el comando 'git reflog'.
+
+
+# • ¿Cómo buscar en el historial de Git?
+#Respuesta: Para buscar en el historial de git usaremos el comando 'git grep'.
+
+
+# • ¿Cómo borrar el historial de Git?
+#Respuesta: Para borrar el historial de Git usaremos el comando 'git reset --hard'.
+
+
+# • ¿Qué es un repositorio privado en GitHub?
+#Respuesta: Un repositorio privado es un repositorio donde solo podran acceder ciertas personas.
+
+
+# • ¿Cómo crear un repositorio privado en GitHub?
+#Respuesta: Para crear un repositorio privado, en el momento de crear un repositorio activaremos la opcion private.
+
+
+# • ¿Cómo invitar a alguien a un repositorio privado en GitHub?
+#Respuesta: Para invitar a alguien nos dirigiremos a la configuracion de nuestro repo y en la seccion 'collaborators', haremos click en el boton que dice 'add people'. 
+
+
+# • ¿Qué es un repositorio público en GitHub?
+#Respuesta: Un repositorio publico es un repositorio donde podra acceder cualquier persona en internet.
+
+
+# • ¿Cómo crear un repositorio público en GitHub?
+#Respuesta: Para crear un repositorio privado, en el momento de crear un repositorio activaremos la opcion public.
+
+
+# • ¿Cómo compartir un repositorio público en GitHub? 
+#Respuesta: Para compartir un repositorio publico basta con copiar el link y mandarselo a quien querramos.
